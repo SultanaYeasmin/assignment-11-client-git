3c93ff74e246045c8f6c09c8eb0cff84d7309c7f
Sultana Yeasmin <EO005092@bifpcl.net>
2025-01-18 19:12:01 +0600

    my-recommendation-delete-part-added

--- src/pages/MyRecommendations.jsx
+++ src/pages/MyRecommendations.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import AuthContext from "../AuthContext/AuthContext";
-import MyRecommendationsTable from "../components/myRecommendationsTable";
+import Swal from "sweetalert2";
+import { MdDeleteForever } from "react-icons/md";
 
 const MyRecommendations = () => {
     const [myRecommendations, setMyRecommendations] = useState([]);
@@ -15,18 +16,52 @@ const MyRecommendations = () => {
             }
             )
     }, [user?.email])
+     const handleDelete = (_id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/myRecommendations/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+
+                        console.log(data)
+                        if (data.deletedCount) {
+                            const restMyRecommendations = myRecommendations.filter(q => q._id !== _id)
+                            setMyRecommendations(restMyRecommendations);
+                            
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your one Recommendation has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
 
     return (
-        <div>
+        <div className="my-20">
             {/* {myRecommendations.length} */}
             <div className="">
             <table className="table table-sm">
-    <thead>
+    <thead className="text-green-800 bg-purple-200">
       <tr>
         <th>Sl. No.</th>
-        <th>User</th>
+        <th>Asked by</th>
         <th>Query</th>
         <th>Suggested product</th>
+        {/* <th>Image</th> */}
         <th>Date</th>
         <th>Remarks</th>
        
@@ -45,7 +80,7 @@ const MyRecommendations = () => {
                           <td className="text-xs">{myRecommendation.queryTitle}</td>
                           <td className="text-xs">{myRecommendation.recommendationName}</td>
                           <td className="text-xs">{myRecommendation. recommendationDate}</td>
-                          <td className="text-xs">X</td>
+                          <td onClick={() => handleDelete(myRecommendation._id)} className="text-red-600"><MdDeleteForever /></td>
                           
                         </tr>
                         
--- src/pages/RecommendationsForMe.jsx
+++ src/pages/RecommendationsForMe.jsx
@@ -16,17 +16,63 @@ const RecommendationsForMe = () => {
     }, [user?.email])
     return (
         <div>
-            {recommendations.length}
-            <div className="grid grid-cols-3 gap-10 ">
-                {
-                    recommendations.map(x => (<p
-                         key={x._id} 
-                         className="border-4">{x.recommendationName}</p>))
+           
+                {/* {myRecommendations.length} */}
+                <div className="container mx-auto px-5 my-10">
+                    <table className="table table-sm">
+                        <thead className="text-green-800 bg-purple-200">
+                            <tr>
+                                <th>Sl. No.</th>
+                                <th>Recommender</th>
+                                <th>My Query</th>
+                                <th>Suggested product</th>
+                                <th>Image</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        {
+                            recommendations?.map((recommendation, index) => <>
 
-                }
+                                <tbody>
+                                    <tr>
+                                        <th className="text-xs">{index + 1}</th>
+                                        <td className="text-xs">{recommendation.recommenderEmail}</td>
+                                        <td className="text-xs">{recommendation.queryTitle}</td>
+                                        <td className="text-xs">{recommendation.recommendationName}</td>
+                                        <td className="text-xs"><img className="w-20 h-14" src={recommendation.recommendationImageURL} /></td>
+                                        <td className="text-xs">{recommendation.recommendationDate}</td>
+
+
+                                    </tr>
+
+                                </tbody>
+                            </>
+
+                            )
+                        }
+                    </table>
+                </div>
             </div>
-        </div>
+
+      
     );
 };
 
 export default RecommendationsForMe;
+
+
+// const newRecommendation = {
+//     recommendationTitle: recommendation_title,
+//     recommendationName: recommendation_name,
+//     recommendationImageURL: recommendation_Image_URL,
+//     recommendationReason: recommendation_reason,
+//     queryId: _id,
+//     queryTitle: query_title,
+//     productName: product_name,
+//     userEmailQuery: user_email,
+//     userNameQuery: user_name,
+//     recommenderEmail: recommender_email,
+//     recommenderName: recommender_name,
+//     recommenderPhoto: recommender_photo,
+//     recommendationDate: current_date,
+// }
